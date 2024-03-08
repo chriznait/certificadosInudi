2a71834efebecc9caa3512de49bb5fb1ae5342ad
christian jordy mamani sucasaire <chriz_nait@hotmail.com>
2024-03-08 16:55:09 -0500

    Creacion de migraciones y modelos de pais y departamento

---
+++ database/migrations/2024_03_08_205124_create_departments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_departamento', 100);
+            $table->string('codigo_departamento', 10)->nullable();
+            $table->string('abreviatura', 10)->nullable();
+            $table->unsignedBigInteger('country_id');
+            $table->integer('flag')->default('1');
+            $table->foreign('country_id')->references('id')->on('countries');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('departments');
+    }
+};
